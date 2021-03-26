2ca77111c05bb9a1011bc68721f902816b4de171
zinebaabbad <55875824+zinebaabbad@users.noreply.github.com>
2021-03-26 11:41:29 +0100

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -3,8 +3,8 @@ import requests
 endpoint="http://127.0.0.1:5000/"
 
 print(requests.get(endpoint+'/home').text)
-print(requests.get(endpoint+"/refreshDatabase").text)
 print(requests.get(endpoint+"/articles/Martin Woollacott, former Guardian foreign editor, dies aged 81 | The Guardian | The Guardian").text)
 print(requests.get(endpoint+"/articles/author/Kari Paul").text)
 print(requests.get(endpoint+"/articles/keyword/suez canal closuresuez canal shipping chartthe ever given").text)
+print(requests.get(endpoint+"/refreshDatabase").text)
 
